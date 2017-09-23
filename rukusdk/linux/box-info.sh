f97a08cbec96be611ab16f2fc61bc66dcff306a0
contact@luclaverdure.com <contact@luclaverdure.com>
2017-09-23 11:11:05 -0400

    Added users and system users

--- rukusdk/linux/box-info.sh
+++ rukusdk/linux/box-info.sh
@@ -1,13 +1,13 @@
-echo -e "** Starting RD_SYS_INFO for Linux"  >> output.txt
+echo -e "** Starting RD_SYS_INFO for Linux" >> output.txt
 echo -e "**LINUX**" >> output.txt
-echo -e "** Getting OS Info..."  >> output.txt
+echo -e "** Getting OS Info..." >> output.txt
 cat /proc/version >> output.txt
-echo -e "** Getting Network Info..."  >> output.txt
+echo -e "** Getting Network Info..." >> output.txt
 ifconfig -a | grep inet >> output.txt
 curl ipinfo.io/ip >> output.txt
 export a=$(curl ipinfo.io/ip)
 traceroute $a >> output.txt
-echo -e "** Getting Processor Info..."  >> output.txt
+echo -e "** Getting Processor Info..." >> output.txt
 lscpu >> output.txt
 grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' >> output.txt
 sleep 1
@@ -20,11 +20,25 @@ sleep 1
 grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' >> output.txt
 sleep 1
 grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' >> output.txt
-echo -e "** Getting Memory Info..."  >> output.txt
+echo -e "** Getting Memory Info..." >> output.txt
 cat /proc/meminfo | grep Mem >> output.txt
-echo -e "** Getting Mounted Disks Info..."  >> output.txt
+echo -e "** Getting Mounted Disks Info..." >> output.txt
 df -h >> output.txt
-echo -e "** Getting Logged In Users..."  >> output.txt
+echo -e "** Getting Logged In Users..." >> output.txt
 who >> output.txt
+echo -e "** Getting All Users..." >> output.txt
+#cut -d: -f1 /etc/passwd >> output.txt
+_l="/etc/login.defs"
+_p="/etc/passwd"
+## get mini UID limit ##
+l=$(grep "^UID_MIN" $_l)
+## get max UID limit ##
+l1=$(grep "^UID_MAX" $_l)
+## use awk to print if UID >= $MIN and UID <= $MAX and shell is not /sbin/nologin   ##
+echo -e "----------[ Normal User Accounts ]---------------" >> output.txt
+awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( $3 >= min && $3 <= max  && $7 != "/sbin/nologin" ) print $0 }' "$_p" >> output.txt
+echo "" >> output.txt
+echo "----------[ System User Accounts ]---------------" >> output.txt
+awk -F':' -v "min=${l##UID_MIN}" -v "max=${l1##UID_MAX}" '{ if ( !($3 >= min && $3 <= max  && $7 != "/sbin/nologin")) print $0 }' "$_p" >> output.txt
 echo ------------------------------------- >> output.txt
 cat output.txt
